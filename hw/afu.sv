1a4553dc9aa3a182e4176744f917584e49ce93dd
Zach Chanak <chanak@wisc.edu>
2021-01-26 21:24:15 -0600

    NO MORE BEGIN ENDD

--- hw/afu.sv
+++ hw/afu.sv
@@ -76,10 +76,9 @@ module afu
   fifo fifo(.clk(clk), .rst_n(rst),.en(rx.c0.mmioWrValid), .d(rx.c0.data), .q(temp));
   
  always @( * ) 
-	begin
 	if (rx.c0.mmioRdValid)
 		tx.c2.data = temp
-end
+
   // assign tx.c2.data = rx.c0.mmioRdValid ? temp : tx.c2.data;
 
    // =============================================================//   
